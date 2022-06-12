aedd815056c42f37577710ff6f5fde2851363f5c
applecdn <86337159+applecdn@users.noreply.github.com>
2022-06-12 16:10:31 +0800

    01

---
+++ app.js
@@ -0,0 +1,69 @@
+let section = document.querySelector("section");
+let add = document.querySelector("form button");
+add.addEventListener("click", (e) => {
+  e.preventDefault();
+  // get the input values
+  //   console.log(e.target.parentElement);
+  let form = e.target.parentElement;
+  //   console.log(form.children);
+  let todoText = form.children[0].value;
+  //   console.log(todoText);
+
+  if (todoText === "") {
+    alert("please input the text.");
+    return;
+  }
+
+  //   create todo
+  let todo = document.createElement("div");
+  todo.classList.add("todo");
+  let text = document.createElement("p");
+  text.classList.add("todo-text");
+  text.innerText = todoText;
+  todo.appendChild(text);
+
+  //   cerat icon
+  let completeButton = document.createElement("button");
+  completeButton.classList.add("complete");
+  completeButton.innerHTML = '<i class="fa-solid fa-check"></i>';
+
+  completeButton.addEventListener("click", (e) => {
+    // console.log(e.target.parentElement);
+    let todoItem = e.target.parentElement;
+    todoItem.classList.toggle("done");
+  });
+
+  //   remove
+  let trashButton = document.createElement("button");
+  trashButton.classList.add("trash");
+  trashButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
+  trashButton.addEventListener("click", (e) => {
+    // console.log(e.target);
+    let todoItem = e.target.parentElement;
+    // console.log(todoItem);
+    todoItem.addEventListener("animationend", () => {
+      todoItem.remove();
+    });
+    todoItem.style.animation = "scaleDown .3s forwards";
+  });
+
+  //   edit
+  let editButton = document.createElement("button");
+  editButton.classList.add("edit");
+  editButton.innerHTML = '<i class="fa-solid fa-pen-to-square"></i>';
+  editButton.addEventListener("click", function () {
+    let innertext = this.parentElement.children[0];
+    let edittext = prompt("edit", innertext.textContent);
+    innertext.textContent = edittext;
+  });
+
+  todo.appendChild(completeButton);
+  todo.appendChild(trashButton);
+  todo.appendChild(editButton);
+
+  todo.style.animation = "scaleUp .3s forwards";
+
+  //   clear input
+  form.children[0].value = "";
+  section.appendChild(todo);
+});
